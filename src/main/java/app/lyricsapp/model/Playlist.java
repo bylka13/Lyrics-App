775e0cce3a7b0ba920814acf0ecaf6b22c9b2212
ziala <zia.lai@etu.univ-amu.fr>
2023-01-31 14:21:58 +0100

    methode addMusic ajout de if et ajout des getteurs

--- src/main/java/app/lyricsapp/model/Playlist.java
+++ src/main/java/app/lyricsapp/model/Playlist.java
@@ -1,6 +1,7 @@
 package app.lyricsapp.model;
 
 import java.util.ArrayList;
+import java.util.Set;
 
 public class Playlist {
     private String playlistName;
@@ -10,9 +11,20 @@ public class Playlist {
         this.listOfMusics = new ArrayList<>();
     }
     public void addMusic(ArrayList<String> Music){
+        if (listOfMusics.contains(Music)){
+
+        }
         listOfMusics.add(Music);
     }
     public void deleteMusic(int pos){
         listOfMusics.remove(pos);
+    }
+
+    public String getPlaylistName() {
+        return playlistName;
+    }
+
+    public ArrayList<ArrayList<String>> getListOfMusics() {
+        return listOfMusics;
     }
 }
