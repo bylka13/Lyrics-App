69f8bd1349e443c49896cd5894a4f9160c0e508f
ziala <zia.lai@etu.univ-amu.fr>
2023-01-31 14:39:16 +0100

    changment des paramettres de addMusic(trois string au lieu d'un arraylist)

--- src/main/java/app/lyricsapp/model/Playlist.java
+++ src/main/java/app/lyricsapp/model/Playlist.java
@@ -4,15 +4,24 @@ import java.util.ArrayList;
 public class Playlist {
     private String playlistName;
     private ArrayList<ArrayList<String>> listOfMusics;
+    // un morceau est une arraylist de trois elements dans cet ordre precis : Titre, Artiste, Paroles;
+    // new Arraylist <"barbamama", "barbapapa", "barbibouille", ...>
     public Playlist (String playlistName){
         this.playlistName = playlistName;
         this.listOfMusics = new ArrayList<>();
     }
-    public void addMusic (ArrayList<String> Music) throws AddMusicException {
+    public void addMusic (String title, String author, String lyrics) throws AddMusicException {
+        ArrayList<String> Music = new ArrayList<>();
+        Music.add(title);
+        Music.add(author);
+        Music.add(lyrics);
+
         if (listOfMusics.contains(Music)){
             throw new AddMusicException;
         }
+        else {
         listOfMusics.add(Music);
+        }
     }
     public void deleteMusic (int pos){
         listOfMusics.remove(pos);
