02c6ec880434d583d7f379819000f6ac45ebbce7
AMAR Amine <amaramine64@gmail.com>
2023-01-31 15:35:18 +0100

    Création de la classe ReadXml et import des bibliothèques

---
+++ src/main/java/app/lyricsapp/model/ReadXml.java
@@ -0,0 +1,11 @@
+package app.lyricsapp.model;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.DocumentBuilder;
+import org.w3c.dom.Document;
+import org.w3c.dom.NodeList;
+import org.w3c.dom.Node;
+import org.w3c.dom.Element;
+import java.io.File;
+
+public class ReadXml {
+}
