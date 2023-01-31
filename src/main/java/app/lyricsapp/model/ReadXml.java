ccb9a8eb982314830dcbe9ac6570c7e3f73fa7ad
khaled <Khaledtiakout85@gmail.com>
2023-01-31 15:44:03 +0100

    parser le fichier xml pour pouvoir le lire et afficher ses données à la console

--- src/main/java/app/lyricsapp/model/ReadXml.java
+++ src/main/java/app/lyricsapp/model/ReadXml.java
@@ -8,4 +8,22 @@ import org.w3c.dom.Element;
 import java.io.File;
 
 public class ReadXml {
+    public static void main(String argv[]) {
+        try {
+            // Constructeur de fichier
+            File xmlFile = new File("src/main/resources/fichiers xml/query1.xml");
+            //Instancier factory
+            DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+            // Analyse du fichier XML
+            DocumentBuilder db = dbf.newDocumentBuilder();
+            Document doc = db.parse(xmlFile);
+            doc.getDocumentElement().normalize();
+            //root node ArrayOfSearchLyricResult
+            System.out.println("Root Element :" + doc.getDocumentElement().getNodeName());
+            System.out.println("*********");
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
