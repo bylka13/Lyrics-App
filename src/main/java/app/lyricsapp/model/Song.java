28ec20b59c3605fd733bee338d6de310559dd0fe
ziala <zia.lai@etu.univ-amu.fr>
2023-01-31 16:30:03 +0100

    classe song

---
+++ src/main/java/app/lyricsapp/model/Song.java
@@ -0,0 +1,27 @@
+package app.lyricsapp.model;
+
+public class Song {
+
+        String title;
+        String author;
+        String lyric;
+
+        public Song ( String title, String author, String lyric){
+            this.title = title;
+            this.author = author;
+            this.lyric = lyric;
+        }
+
+        public String getAuthor() {
+            return author;
+        }
+
+        public String getTitle() {
+            return title;
+        }
+
+        public String getLyric() {
+            return lyric;
+        }
+    }
+}
