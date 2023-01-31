aab348fcd716eed702c0a54037380bd9f2853507
ziala <zia.lai@etu.univ-amu.fr>
2023-01-31 14:06:44 +0100

    constructeur classe playlist + changement de type de l'attribut listOfMusic en ArrayList<ArrayList<String>>

--- src/main/java/app/lyricsapp/model/Playlist.java
+++ src/main/java/app/lyricsapp/model/Playlist.java
@@ -1,6 +1,13 @@
 package app.lyricsapp.model;
 
+import java.util.ArrayList;
+
 public class Playlist {
     private String playlistName;
-    private String[][] listOfMusics;
+    private ArrayList<ArrayList<String>> listOfMusics;
+    public Playlist (String playlistName){
+        this.playlistName = playlistName;
+        this.listOfMusics = new ArrayList<>();
+    }
+
 }
