599e895373794c126df5abd9749e97b93a40ca2c
g21209074 <florent.gilloteaux@etu.univ-amu.fr>
2023-01-31 14:08:26 +0100

    implementation getters

--- src/main/java/app/lyricsapp/model/Playlist.java
+++ src/main/java/app/lyricsapp/model/Playlist.java
@@ -3,4 +3,18 @@ package app.lyricsapp.model;
 public class Playlist {
     private String playlistName;
     private String[][] listOfMusics;
+
+
+
+
+
+
+    public String getPlaylistName() {
+        return playlistName;
+    }
+
+    public String[][] getListOfMusics() {
+        return listOfMusics;
+    }
+
 }
