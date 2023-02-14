693f5745ef52dacbcac58ba4dba8d33ade0f46ff
AMAR Amine <amaramine64@gmail.com>
2023-02-14 15:02:33 +0100

    Ajout de constructeur et d'attributs pour la classe LyricSongDirect

--- src/main/java/app/lyricsapp/model/LyricSongDirect.java
+++ src/main/java/app/lyricsapp/model/LyricSongDirect.java
@@ -1,5 +1,27 @@
 package app.lyricsapp.model;
 
 public class LyricSongDirect {
+    private int TrackId;
+    private String LyricChecksum;
+    private int LyricId;
+    private String LyricSong;
+    private String LyricArtist;
+    private String LyricUrl;
+    private String LyricCovertArtUrl;
+    private int LyricRank;
+    private String LyricCorrectUrl;
+    private String Lyric;
 
+    public LyricSongDirect(int trackId, String lyricChecksum, int lyricId, String lyricSong, String lyricArtist, String lyricUrl, String lyricCovertArtUrl, int lyricRank, String lyricCorrectUrl, String lyric) {
+        TrackId = trackId;
+        LyricChecksum = lyricChecksum;
+        LyricId = lyricId;
+        LyricSong = lyricSong;
+        LyricArtist = lyricArtist;
+        LyricUrl = lyricUrl;
+        LyricCovertArtUrl = lyricCovertArtUrl;
+        LyricRank = lyricRank;
+        LyricCorrectUrl = lyricCorrectUrl;
+        Lyric = lyric;
+    }
 }
