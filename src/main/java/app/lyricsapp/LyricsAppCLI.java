2cb9e2dec05fd4442d8b8d291a07b2993c3364d4
k21221005 <mehdi.khaled@etu.univ-amu.fr>
2023-02-14 15:04:35 +0100

    fin de la navigation sur la ligne de commande

--- src/main/java/app/lyricsapp/LyricsAppCLI.java
+++ src/main/java/app/lyricsapp/LyricsAppCLI.java
@@ -11,14 +11,32 @@ public class LyricsAppCLI {
     public static void main(String[] args) throws IOException, ParserConfigurationException, SAXException {
         System.out.println("Welcome to the lyrics app");
         while (true) {
-            System.out.println("\nInput your command: ");
-            System.out.println("Menu: " +
-                    "\nRecherche de chansons: " +
-                    "\n1/ Par Artiste et par Titre " +
-                    "\n2/ Par Paroles " +
-                    "\n0/ Pour quitter");
+            System.out.println("Input your command: ");
+            System.out.println("Menu: \n" + "Recherche de chansons: \n" + "1/ Par Artiste et par Titre \n" + "2/ Par Paroles \n" + "0/ Pour quitter");
             Scanner scanner = new Scanner(System.in);
             String input = scanner.nextLine();
+
+            if (input.equals("1")) {
+                System.out.println("Veuillez entrer le nom de l'artiste \n");
+                Scanner scanner1 = new Scanner(System.in);
+                String artist = scanner1.nextLine();
+                System.out.println("Veuillez entrer le titre de votre chanson \n");
+                Scanner scanner2 = new Scanner(System.in);
+                String titre = scanner2.nextLine();
+                SearchLyricDirect.searchLyricDirect(artist, titre);
+            }
+
+            if (input.equals("2")) {
+                System.out.println("Veuillez entrer vos paroles \n");
+                Scanner scanner3 = new Scanner(System.in);
+                String paroles = scanner3.nextLine();
+                SearchLyricText.searchLyricText(paroles);
+            }
+
+
+            if (input.equals("0")) {
+                break;
+            }
         }
     }
 }
