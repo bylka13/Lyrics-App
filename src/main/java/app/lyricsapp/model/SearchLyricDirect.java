27d885a123a714209ba8d4bf77aaa425cb48766c
AMAR Amine <amaramine64@gmail.com>
2023-02-14 17:09:08 +0100

    recupération des informations depuis l'api pour la classe searchLyricDirect

--- src/main/java/app/lyricsapp/model/SearchLyricDirect.java
+++ src/main/java/app/lyricsapp/model/SearchLyricDirect.java
@@ -16,16 +16,9 @@ public class SearchLyricDirect{
 
         String lien = "http://api.chartlyrics.com/apiv1.asmx/SearchLyricDirect?artist=" + artist + "&song=" + title;
 
-        int TrackId = 0;
-        String LyricChecksum = null;
-        int LyricId = 0;
         String LyricArtist = null;
         String LyricSong = null;
-        String LyricUrl = null;
-        String LyricCovertArtUrl = null;
-        String LyricCorrectUrl = null ;
         String Lyric = null;
-        int LyricRank = 0;
 
         DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
         DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
@@ -40,38 +33,17 @@ public class SearchLyricDirect{
         for(int i = 0; i < nodeList.getLength(); i++) {
 
             Node newNode = nodeList.item(i);
-            if (newNode.getNodeName().equals("TrackId")) {
-                TrackId = Integer.parseInt(newNode.getTextContent());
-            }
-            if (newNode.getNodeName().equals("LyricChecksum")) {
-                LyricChecksum = newNode.getTextContent();
-            }
-            if (newNode.getNodeName().equals("LyricId")) {
-                LyricId = Integer.parseInt(newNode.getTextContent());
-            }
             if (newNode.getNodeName().equals("LyricSong")) {
                 LyricSong = newNode.getTextContent();
             }
             if (newNode.getNodeName().equals("LyricArtist")){
                 LyricArtist = new String(newNode.getTextContent());
             }
-            if (newNode.getNodeName().equals("LyricUrl")) {
-                LyricUrl = newNode.getTextContent();
-            }
-            if (newNode.getNodeName().equals("LyricCovertArtUrl")) {
-                LyricCovertArtUrl = newNode.getTextContent();
-            }
-            if (newNode.getNodeName().equals("LyricRank")) {
-                LyricRank = Integer.parseInt(newNode.getTextContent());
-            }
-            if (newNode.getNodeName().equals("LyricCorrectUrl")) {
-                LyricCorrectUrl = newNode.getTextContent();
-            }
             if (newNode.getNodeName().equals("Lyric")) {
                 Lyric = newNode.getTextContent();
             }
         }
-        System.out.println(new LyricSongDirect(TrackId, LyricChecksum, LyricId, LyricSong, LyricArtist, LyricUrl, LyricCovertArtUrl, LyricRank, LyricCorrectUrl, Lyric));
+        System.out.println(new LyricSongDirect(LyricSong, LyricArtist, Lyric));
 
     }
 
