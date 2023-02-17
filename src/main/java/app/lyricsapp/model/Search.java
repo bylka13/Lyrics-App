b5b978c13a780528ffe74baccc00cd3ea60ee1ca
khaled <Khaledtiakout85@gmail.com>
2023-02-17 15:24:14 +0100

    generalisation de la classe search

--- src/main/java/app/lyricsapp/model/Search.java
+++ src/main/java/app/lyricsapp/model/Search.java
@@ -9,10 +9,69 @@ import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
 import java.io.IOException;
-
+import java.util.ArrayList;
 
 
 public class Search {
+
+    ArrayList<Song> toPrint;
+
+    public Search() {
+        this.toPrint = new ArrayList<>();
+    }
+
+    public ArrayList<Song> getToPrint() {
+        return toPrint;
+    }
+
+    public void searchLyricText(String lyrics, int numberOfResults) throws IOException, SAXException, ParserConfigurationException {
+
+        String Artist = null;
+        String Song = null;
+        String SongUrl = null;
+
+        String lien = "http://api.chartlyrics.com/apiv1.asmx/SearchLyricText?lyricText=" + lyrics;
+
+        DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+        Document document = dBuilder.parse(lien);
+
+        NodeList booksList = document.getElementsByTagName("ArrayOfSearchLyricResult");
+
+        Node node = booksList.item(0);
+
+        NodeList nodeList = node.getChildNodes();
+
+        for(int i = 1; i < nodeList.getLength(); i++) {
+
+            Node newNode = nodeList.item(i);
+            NodeList newNodeList = newNode.getChildNodes();
+            for(int j = 1; j < newNodeList.getLength(); j++) {
+
+                Node nodeToCheck = newNodeList.item(j);
+                if (nodeToCheck.getNodeName().equals("SongUrl")) {
+                    SongUrl = newNodeList.item(j).getTextContent();
+                }
+                if (nodeToCheck.getNodeName().equals("Artist")) {
+                    Artist = newNodeList.item(j).getTextContent();
+                }
+                if (nodeToCheck.getNodeName().equals("Song")) {
+                    Song = newNodeList.item(j).getTextContent();
+                }
+            }
+
+            Song music = new Song(SongUrl, Artist, Song);
+            if(!toPrint.contains(music)){
+                toPrint.add(music);
+            }
+        }
+        if(numberOfResults < 1 || numberOfResults > 24){
+            System.out.println("Veuillez saisir un nombre compris entre 1 et 24 inclus.");
+        }
+        for(int i = 1; i <= numberOfResults; i++){
+            System.out.println(i + ". " + toPrint.get(i));
+        }
+    }
     public static void searchLyricDirect(String artist, String title) throws IOException, ParserConfigurationException, SAXException {
 
         String lien = "http://api.chartlyrics.com/apiv1.asmx/SearchLyricDirect?artist=" + artist + "&song=" + title;
