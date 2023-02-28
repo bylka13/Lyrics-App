135d336482ab591e8070e0fadbfd6caaf7c19d0d
g21209074 <florent.gilloteaux@etu.univ-amu.fr>
2023-02-28 16:55:04 +0100

    inversion arguments de creation d'objet musique (lyric et tilte etaient inversés)

--- src/main/java/app/lyricsapp/model/Search.java
+++ src/main/java/app/lyricsapp/model/Search.java
@@ -105,7 +105,7 @@ public class Search {
                 Lyric = newNode.getTextContent();
             }
         }
-        return new Song(LyricSong, LyricArtist,Lyric);
+        return new Song(Lyric, LyricArtist,LyricSong);
 
     }
 }
