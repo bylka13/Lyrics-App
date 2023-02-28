dc68fc1f7f0b464654a91793f6aa573d6688e15b
khaled <Khaledtiakout85@gmail.com>
2023-02-28 15:08:57 +0100

    optimisation de la classe LyricsApp.java

--- src/main/java/app/lyricsapp/LyricsApp.java
+++ src/main/java/app/lyricsapp/LyricsApp.java
@@ -1,23 +1,27 @@
 package app.lyricsapp;
 
 import javafx.application.Application;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
+
+import java.awt.event.ActionEvent;
+import java.io.IOException;
+import java.util.Objects;
 
 
 public class LyricsApp extends Application {
-
     @Override
     public void start(Stage primaryStage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("/app/lyricsapp/view/lyricsapp.fxml"));
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/app/lyricsapp/view/lyricsapp.fxml")));
         primaryStage.setTitle("LyricsApp");
 
         primaryStage.setScene(new Scene(root));
         primaryStage.show();
 
     }
-
     public static void main(String[] args) { launch(args); }
 }
