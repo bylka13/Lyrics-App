24600a1f73c93f4e1087e4d1aaa492107c9fc2c8
florent <florent.gilloteaux@etu.univ-amu.fr>
2023-03-15 15:02:36 +0100

    Ajout fonctionnalité ajout/supression d'une musique dans les favoris + finalisation de la recherche par parole

--- src/main/java/app/lyricsapp/LyricsAppCLI.java
+++ src/main/java/app/lyricsapp/LyricsAppCLI.java
@@ -17,9 +17,9 @@ public class LyricsAppCLI {
         File file = new File("src/main/resources/fichiers xml/favorites.xml");
         if (file.exists()) {
             favorites.addAllMusics(ReadXml.readXml("src/main/resources/fichiers xml/favorites.xml"));
-            System.out.println("Le fichier favoris a été chargé");
+            System.out.println("Le fichier favoris a été chargé \n");
         }
-        System.out.println("Welcome to the lyrics app");
+        System.out.println("Welcome to the lyrics app \n");
         while (true) {
             System.out.println("Input your command: ");
             System.out.println("Menu: \n" +
@@ -27,60 +27,102 @@ public class LyricsAppCLI {
                                "1/ Par Artiste et par Titre \n" +
                                "2/ Par Paroles \n" +
                                "3/ Accéder aux favoris \n" +
-                               "0/ Pour quitter");
+                               "0/ Pour quitter \n");
             Scanner scanner = new Scanner(System.in);
             String input = scanner.nextLine();
 
             if(Objects.equals(input, "1")){
-                System.out.println("Veuillez entrer le nom de l'artiste :");
+                System.out.println("Veuillez entrer le nom de l'artiste : \n");
                 Scanner scanner1 = new Scanner(System.in);
                 String artist = scanner1.nextLine();
-                System.out.println("Veuillez entrer le titre de votre chanson :");
+                System.out.println("Veuillez entrer le titre de votre chanson : \n");
                 Scanner scanner2 = new Scanner(System.in);
                 String titre = scanner2.nextLine();
                 Song song = Search.searchLyricDirect(artist,titre);
-                System.out.println(Search.searchLyricDirect(artist,titre));
+                System.out.println(song);
                 Scanner scanner3 = new Scanner(System.in);
-                System.out.println("Voulez-vous ajouter ce son dans la liste des favoris ?");
+                System.out.println("Voulez-vous ajouter ce son dans la liste des favoris ? \n");
                 System.out.println("1/ Oui \n" +
                                    "2/ Non");
                 if(Objects.equals(scanner3.nextLine(), "1")){
                     favorites.addMusic(song);
                     SaveFavoritesXML.createDocument(favorites);
-
+                }
+                else {
+                    continue;
                 }
 
             }
 
             if(Objects.equals(input, "2")) {
-                System.out.println("Veuillez entrer vos paroles :");
+                System.out.println("Veuillez entrer vos paroles : \n");
                 Scanner scanner3 = new Scanner(System.in);
                 String paroles = scanner3.nextLine();
-                System.out.println("Veuillez entrer le nombre de chansons que vous voulez :");
+                System.out.println("Veuillez entrer le nombre de chansons que vous voulez : \n");
                 Scanner scanner4 = new Scanner(System.in);
                 int numberOfResults = Integer.parseInt(scanner4.nextLine());
                 while (numberOfResults < 1 || numberOfResults > 24) {
-                    System.out.println("Veuillez entrer un nombre compris entre 1 et 24 inclus :");
+                    System.out.println("Veuillez entrer un nombre compris entre 1 et 24 inclus : \n");
                     Scanner scanner5 = new Scanner(System.in);
                     numberOfResults = Integer.parseInt(scanner5.nextLine());
                 }
                 mySearch.searchLyricText(paroles, numberOfResults);
-                System.out.println("Veuillez entrer le numéro de la chanson que vous voulez afficher:");
+                System.out.println("Veuillez entrer le numéro de la chanson que vous voulez afficher: \n");
                 Scanner scanner6 = new Scanner(System.in);
                 int numberOfTheSong = Integer.parseInt(scanner6.nextLine());
                 int number = numberOfResults;
                 while (numberOfTheSong < 1 || numberOfTheSong > numberOfResults) {
-                    System.out.println("Veuillez entrer un nombre compris entre 1 et " + number + " inclus :");
+                    System.out.println("Veuillez entrer un nombre compris entre 1 et " + number + " inclus : \n");
                     Scanner scanner7 = new Scanner(System.in);
                     numberOfTheSong = Integer.parseInt(scanner7.nextLine());
                 }
                 String artiste = mySearch.getToPrint().get(numberOfTheSong).getAuthor();
                 String title = mySearch.getToPrint().get(numberOfTheSong).getTitle();
-                Search.searchLyricDirect(artiste, title);
+                Song song2 = Search.searchLyricDirect(artiste, title);
+                System.out.println(song2);
+                System.out.println("Voulez-vous ajouter ce son dans la liste des favoris ? \n");
+                System.out.println("1/ Oui \n" +
+                                   "2/ Non \n");
+                Scanner scanner8 = new Scanner(System.in);
+                int indexOfMusic = Integer.parseInt(scanner8.nextLine());
+                if (indexOfMusic == 1){
+                    favorites.addMusic(song2);
+                    SaveFavoritesXML.createDocument(favorites);
+                    System.out.println("La chanson a bien été ajoutée. \n");
+                }
+                else {
+                    continue;
+                }
+
             }
 
             if(Objects.equals(input, "3")) {
                 favorites.display();
+                System.out.println("Input your command: ");
+                System.out.println("Menu: \n" +
+                        "Recherche de chansons: \n" +
+                        "1/ Afficher une chanson \n" +
+                        "2/ Supprimer une chanson \n" +
+                        "0/ Pour retourner au menu principal \n");
+                Scanner scanner9 = new Scanner(System.in);
+                String input2 = scanner9.nextLine();
+                if (Objects.equals(input2, "1")){
+                    System.out.println("Selectionnez la chanson à afficher en détails (entrez un nombre): \n");
+                    Scanner scanner10 = new Scanner(System.in);
+                    int indexOfMusic = Integer.parseInt(scanner10.nextLine());
+                    System.out.println(favorites.getASong(indexOfMusic - 1));
+                }
+
+                if (Objects.equals(input2, "2")){
+                    System.out.println("Selectionnez la chanson à supprimer (entrez un nombre) : \n");
+                    Scanner scanner11 = new Scanner(System.in);
+                    int indexOfMusic2 = Integer.parseInt(scanner11.nextLine());
+                    favorites.deleteASong(indexOfMusic2 - 1);
+                }
+
+                else {
+                    continue;
+                }
             }
 
 
