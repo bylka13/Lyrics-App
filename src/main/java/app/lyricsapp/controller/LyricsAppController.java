3fc6e6ae795734fbc2790ab5b52aaea2d2b4e6c3
AMAR Amine <amaramine64@gmail.com>
2023-03-17 17:54:08 +0100

    implémentation du design de l'interface graphique

--- src/main/java/app/lyricsapp/controller/LyricsAppController.java
+++ src/main/java/app/lyricsapp/controller/LyricsAppController.java
@@ -1,24 +1,61 @@
 package app.lyricsapp.controller;
 
-import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.fxml.Initializable;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.VBox;
-
-import java.awt.event.ActionEvent;
-import java.io.IOException;
+import javafx.scene.control.*;
+import javafx.scene.layout.*;
+import javafx.scene.paint.Color;
 import java.net.URL;
-import java.util.Objects;
+import app.lyricsapp.model.*;
+import javafx.fxml.*;
 import java.util.ResourceBundle;
 
 public class LyricsAppController implements Initializable {
+    @FXML
+    private Button helpButton;
+    @FXML
+    private Label LyricsTitle;
+    @FXML
+    private Button myFavButton;
+    @FXML
+    private Button searchLyricDirectButton;
+    @FXML
+    private Button searchLyricTextButton;
+    @FXML
+    private Spinner<Integer> numberOfResults;
+    @FXML
+    private TextField inputTitle;
+    @FXML
+    private TextField inputArtist;
+    @FXML
+    private TextField inputLyrics;
+    @FXML
+    private GridPane gridPane;
 
+
+    Playlist favorites = new Playlist("favorites");
 
     @Override
-    public void initialize(URL location, ResourceBundle resourceBundle) {}
+    public void initialize(URL location, ResourceBundle resourceBundle) {
+
+        SpinnerValueFactory<Integer> valueFactory = new SpinnerValueFactory.IntegerSpinnerValueFactory(1, 25, 1);
+        numberOfResults.setValueFactory(valueFactory);
+
+        CornerRadii buttonCornerRadii = new CornerRadii(50, 50, 50, 50, false);
+        BackgroundFill buttonBackgroundFill = new BackgroundFill(Color.GREEN, buttonCornerRadii, null);
+        Background buttBackground = new Background(buttonBackgroundFill);
+
+        myFavButton.setBackground(buttBackground);
+        searchLyricTextButton.setBackground(buttBackground);
+        searchLyricDirectButton.setBackground(buttBackground);
+        helpButton.setBackground(buttBackground);
+
+        myFavButton.setTextFill(Color.WHITE);
+        searchLyricTextButton.setTextFill(Color.WHITE);
+        searchLyricDirectButton.setTextFill(Color.WHITE);
+        helpButton.setTextFill(Color.WHITE);
 
 
+        LyricsTitle.setStyle("-fx-background-color: green; -fx-text-fill: white");
+
+
+    }
 }
