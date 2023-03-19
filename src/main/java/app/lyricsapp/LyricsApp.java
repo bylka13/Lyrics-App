cf47356c6761cd13c6fa2536bfbe0e994a70be74
TIAKOUT Khaled <khaled.tiakout@etu.univ-amu.fr>
2023-03-19 15:28:10 +0000

    Update LyricsApp.java

--- src/main/java/app/lyricsapp/LyricsApp.java
+++ src/main/java/app/lyricsapp/LyricsApp.java
@@ -8,7 +8,6 @@ import javafx.scene.Scene;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
-import java.awt.event.ActionEvent;
 import java.io.IOException;
 import java.util.Objects;
 
